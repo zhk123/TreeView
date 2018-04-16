f0b098e5e3020003da5a2b91670f04457a8f12b9
ZHK <934393901@qq.com>
2018-04-16 16:12:57 +0800

    add

--- app/src/main/java/com/zk/treeview/MainActivity.java
+++ app/src/main/java/com/zk/treeview/MainActivity.java
@@ -2,9 +2,6 @@ package com.zk.treeview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.zk.mylibrary.CustomUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String name = CustomUtil.getName();
-        Log.i("TAG_lib", "onCreate: " + name);
+//        String name = CustomUtil.getName();
+//        Log.i("TAG_lib", "onCreate: " + name);
     }
 }
